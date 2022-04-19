e3df41466afd76fccb991936e1806caed8506ac0
duchoang0103 <hoangkimanhduc01031999@gmail.com>
2022-04-20 00:44:04 +0700

    upload quan ly nhan vien backend dung strapi

---
+++ src/api/nhanvien/routes/nhanvien.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * nhanvien router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::nhanvien.nhanvien');
---
+++ src/api/nhanvien/services/nhanvien.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * nhanvien service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::nhanvien.nhanvien');
